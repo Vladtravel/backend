64620687aef1de151e479571b6d941fd36c4ca55
Vladyslav <vladtravel2015@gmail.com>
2021-11-12 11:07:47 +0200

    add acces mongo

--- model/schemas/user.js
+++ model/schemas/user.js
@@ -1,86 +1,85 @@
-// const mongoose = require("mongoose");
-// const { Schema, model } = mongoose;
-// const gravatar = require("gravatar");
-// const shortid = require("shortid");
-// const bcrypt = require("bcryptjs");
-// const SALT_FACTOR = 6;
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+const gravatar = require("gravatar");
+const shortid = require("shortid");
+const bcrypt = require("bcryptjs");
+const SALT_FACTOR = 6;
 
-// const userSchema = new Schema(
-//   {
-//     name: {
-//       type: String,
-//       required: true,
-//     },
+const userSchema = new Schema(
+  {
+    // name: {
+    //   type: String,
+    //   required: true,
+    // },
+    email: {
+      type: String,
+      required: ["true", "Email is required"],
+      unique: true,
+      validate(value) {
+        const reg = /\S+@\S+\.\S+/;
+        return reg.test(String(value).toLowerCase());
+      },
 
-//     password: {
-//       type: String,
-//       required: ["true", "Password is required"],
-//     },
+      password: {
+        type: String,
+        required: ["true", "Password is required"],
+      },
+    },
 
-//     email: {
-//       type: String,
-//       required: ["true", "Email is required"],
-//       unique: true,
-//       validate(value) {
-//         const reg = /\S+@\S+\.\S+/;
-//         return reg.test(String(value).toLowerCase());
-//       },
-//     },
+    // subscription: {
+    //   type: String,
+    //   enum: ["starter", "pro", "business"],
+    //   default: "starter",
+    // },
 
-//     subscription: {
-//       type: String,
-//       enum: ["starter", "pro", "business"],
-//       default: "starter",
-//     },
+    token: {
+      type: String,
+      default: null,
+    },
 
-//     token: {
-//       type: String,
-//       default: null,
-//     },
+    avatarURL: {
+      type: String,
+      default: function () {
+        return gravatar.url(this.email, { s: "250" }, true);
+      },
+    },
 
-//     avatarURL: {
-//       type: String,
-//       default: function () {
-//         return gravatar.url(this.email, { s: "250" }, true);
-//       },
-//     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verifyToken: {
+      type: String,
+      required: [true, "Verify token is required"],
+      default: shortid.generate(),
+    },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toObject: { virtuals: true },
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+);
 
-//     verify: {
-//       type: Boolean,
-//       default: false,
-//     },
-//     verifyToken: {
-//       type: String,
-//       required: [true, "Verify token is required"],
-//       default: shortid.generate(),
-//     },
-//   },
-//   {
-//     versionKey: false,
-//     timestamps: true,
-//     toObject: { virtuals: true },
-//     toJSON: {
-//       virtuals: true,
-//       transform: function (doc, ret) {
-//         delete ret._id;
-//         return ret;
-//       },
-//     },
-//   }
-// );
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    const salt = await bcrypt.genSalt(SALT_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+  }
+  next();
+});
 
-// userSchema.pre("save", async function (next) {
-//   if (this.isModified("password")) {
-//     const salt = await bcrypt.genSalt(SALT_FACTOR);
-//     this.password = await bcrypt.hash(this.password, salt);
-//   }
-//   next();
-// });
+userSchema.methods.validPassword = async function (password) {
+  return await bcrypt.compare(String(password), this.password);
+};
 
-// userSchema.methods.validPassword = async function (password) {
-//   return await bcrypt.compare(String(password), this.password);
-// };
+const User = model("user", userSchema);
 
-// const User = model("user", userSchema);
-
-// module.exports = User;
+module.exports = User;
--- repository/users.js
+++ repository/users.js
@@ -1,45 +1,45 @@
-// const User = require("../model/schemas/user");
+const User = require("../model/schemas/user");
 
-// const findByEmail = async (email) => {
-//   return await User.findOne({ email });
-// };
+const findByEmail = async (email) => {
+  return await User.findOne({ email });
+};
 
-// const findById = async (id) => {
-//   return await User.findOne({ _id: id });
-// };
+const findById = async (id) => {
+  return await User.findOne({ _id: id });
+};
 
-// const findByVerifyTokenEmail = async (token) => {
-//   return await User.findOne({ verifyToken: token });
-// };
+const findByVerifyTokenEmail = async (token) => {
+  return await User.findOne({ verifyToken: token });
+};
 
-// const create = async (userOptions) => {
-//   const user = new User(userOptions);
-//   return await user.save();
-// };
+const create = async (userOptions) => {
+  const user = new User(userOptions);
+  return await user.save();
+};
 
-// const updateToken = async (id, token) => {
-//   return await User.updateOne({ _id: id }, { token });
-// };
+const updateToken = async (id, token) => {
+  return await User.updateOne({ _id: id }, { token });
+};
 
-// const updateSubUser = async (id, subscription) => {
-//   return await User.updateOne({ _id: id }, { subscription });
-// };
+const updateSubUser = async (id, subscription) => {
+  return await User.updateOne({ _id: id }, { subscription });
+};
 
-// const updateAvatar = async (id, avatar, idCloudAvatar = null) => {
-//   return await User.updateOne({ _id: id }, { avatar, idCloudAvatar });
-// };
+const updateAvatar = async (id, avatar, idCloudAvatar = null) => {
+  return await User.updateOne({ _id: id }, { avatar, idCloudAvatar });
+};
 
-// const updateVerifyToken = async (id, verify, verifyToken) => {
-//   return await User.updateOne({ _id: id }, { verify, verifyToken: verifyToken });
-// };
+const updateVerifyToken = async (id, verify, verifyToken) => {
+  return await User.updateOne({ _id: id }, { verify, verifyToken: verifyToken });
+};
 
-// module.exports = {
-//   findByEmail,
-//   create,
-//   findById,
-//   updateToken,
-//   updateSubUser,
-//   updateAvatar,
-//   findByVerifyTokenEmail,
-//   updateVerifyToken,
-// };
+module.exports = {
+  findByEmail,
+  create,
+  findById,
+  updateToken,
+  updateSubUser,
+  updateAvatar,
+  findByVerifyTokenEmail,
+  updateVerifyToken,
+};
