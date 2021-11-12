354993494680a58c2612333bf78d24870460a2b1
Vladyslav <vladtravel2015@gmail.com>
2021-11-12 11:28:05 +0200

    update mongo acces

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-const uriDb = process.env.DB_ACCES;
+const uriDb = process.env.DB_HOST;
+console.log(uriDb);
 
 const db = mongoose.connect(uriDb, {
   useNewUrlParser: true,
